7926ca72d386be33d4fca9fb5f4b8cf24bdd7804
AnSuHan <glorygem195@gmail.com>
2024-02-22 14:32:01 +0900

    Find next cycle step

--- Slither_Project/lib/GameSceneStateSquare.dart
+++ Slither_Project/lib/GameSceneStateSquare.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'GameSceneSquare.dart';
@@ -77,7 +79,7 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
 
   //배열에 담긴 위젯을 column에 담아서 widget으로 반환
   static List<Widget> buildSquarePuzzle(width, height) {
-    List<List<Widget>> puzzle = initSquarePuzzle(width, height);
+    List<List<SquareBox>> puzzle = initSquarePuzzle(width, height);
     List<Widget> columnChildren = [];
 
     for (int i = 0; i < height; i++) {
@@ -92,7 +94,114 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
         ),
       );
     }
+    // puzzle[0][2].down = 1;
+    setPuzzle(width, height, puzzle);
 
     return columnChildren;
+  }
+
+  static void setPuzzle(width, height, List<List<SquareBox>> puzzle) {
+    //cycle이 저장될 배열
+    List<List<int>> answer = List.generate(height, (index) {
+      if (index % 2 == 0) {
+        return List<int>.filled(width, 0);
+      } else {
+        return List<int>.filled(width + 1, 0);
+      }
+    });
+
+    //배열 초기화
+    for(int i = 0 ; i < height ; i++) {
+      for(int j = 0 ; j < ((i % 2) == 0 ? width : (width + 1)) ; j++) {
+        answer[i][j] = 0;
+      }
+    }
+
+    //print("answer : ${answer.length}"); //5
+    //print("answer : ${answer[1].length}"); //10 or 11
+
+    var startRow = Random().nextInt(answer.length);
+    var startColumn = Random().nextInt(answer[startRow].length);
+    var currentRow = startRow;
+    var currentColumn = startColumn;
+
+    var nextDirection  = -1; //0 : up, 1 : down, 2 : left, 3 : right
+    var nextDirectionList = [0, 1, 2, 3];
+
+    // print("row $nextRow, col $nextColumn");
+    // print("width $width, height $height");
+
+    answer[currentRow][currentColumn] = 1;
+
+    //cycle이 완성될 때까지 반복
+    while(true) {
+      //방향 변수 초기화
+      nextDirectionList = [0, 1, 2, 3];
+
+      //진행 방향을 설정
+      do {
+        nextDirection = nextDirectionList[nextDirectionList.isNotEmpty ? Random().nextInt(nextDirectionList.length) : 0];
+        //최대 4번 내에 프로세스가 종료되도록 나온 값을 제거
+        nextDirectionList.remove(nextDirection);
+
+        print("nextDirection $nextDirection, nextRow : $currentRow, nextColumn : $currentColumn");
+
+        //다음에 갈 셀의 값이 1이면 다시 반복해야 함 (잘못된 사이클이 생성)
+        var temp = [0, 0];
+        switch(nextDirection) {
+          case 0:
+            temp[0] = -1;
+            break;
+          case 1:
+            temp[0] = 1;
+            break;
+          case 2:
+            temp[1] = -1;
+            break;
+          default:
+            temp[1] = -1;
+        }
+        //while에 걸린 조건을 통과한 경우에만 검사
+        if(!((nextDirection == 0 && currentRow == 0) ||
+            (nextDirection == 1 && currentRow == height) ||
+            (nextDirection == 2 && currentColumn == 0) ||
+            (nextDirection == 3 && currentColumn == width))) {
+          if(answer[currentRow + temp[0]][currentColumn + temp[1]] == 1) {
+            continue;
+          }
+        }
+      } while(
+          (nextDirection == 0 && currentRow == 0) ||
+          (nextDirection == 1 && currentRow == height) ||
+          (nextDirection == 2 && currentColumn == 0) ||
+          (nextDirection == 3 && currentColumn == width)
+      );  //진행 가능한 방향이 나올 때까지 while
+
+      //진행 방향을 적용
+      switch(nextDirection) {
+        case 0:
+          currentRow -= 1;
+          break;
+        case 1:
+          currentRow += 1;
+          break;
+        case 2:
+          currentColumn -= 1;
+          break;
+        default:
+          currentColumn += 1;
+      }
+
+      //사이클 종료 조건
+      if((currentRow == startRow) && (currentColumn == startColumn)) {
+        break;
+      }
+
+      answer[currentRow][currentColumn] = 1;
+      print(answer);
+
+      //cycle이 완성 되었는지 판단
+      break;
+    }
   }
 }
