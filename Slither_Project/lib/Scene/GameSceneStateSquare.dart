fe505862b9541a9f33fe72c3ec165e779bef1d45
AnSuHan <glorygem195@gmail.com>
2024-05-30 15:26:24 +0900

    support dynamic size

--- Slither_Project/lib/Scene/GameSceneStateSquare.dart
+++ Slither_Project/lib/Scene/GameSceneStateSquare.dart
@@ -11,6 +11,7 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
   late Size screenSize;
   static const puzzleWidth = 20;    //num of Square - horizontal
   static const puzzleHeight = 10;   //num of Square - vertical
+  static const numOfEdge = 4;
   static late List<Widget> squareField;
   static late List<List<SquareBox>> puzzle;
   var findCycle = false;
@@ -20,7 +21,19 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
   void initState() {
     super.initState();
     _setupKeyListener();
+    //dummy init for avoiding null
     squareField = buildSquarePuzzle(puzzleWidth, puzzleHeight);
+    loadPuzzle();
+  }
+
+  void loadPuzzle() async {
+    List<List<int>> data = await ReadSquare().loadPuzzle();
+    squareField = buildSquarePuzzle(data[0].length, data.length ~/ 2);
+    List<Widget> newSquareField = await buildSquarePuzzleAnswer(data);
+
+    setState(() {
+      squareField = newSquareField;
+    });
   }
 
   void _setupKeyListener() {
@@ -137,6 +150,7 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
   //배열에 담긴 위젯을 column에 담아서 widget으로 반환
   static List<Widget> buildSquarePuzzle(width, height) {
     List<List<SquareBox>> puzzle = initSquarePuzzle(width, height);
+    //print("puzzle row ${puzzle.length}, col ${puzzle[0].length}");
     List<Widget> columnChildren = [];
 
     for (int i = 0; i < height; i++) {
@@ -155,14 +169,22 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
     return columnChildren;
   }
 
-  static Future<List<Widget>> buildSquarePuzzleAnswer(List<List<int>> answer, {int progress = 1}) async {
-    //check progress
+  static Future<List<Widget>> buildSquarePuzzleAnswer(List<List<int>> answer) async {
+    /*
+    //정상
+    for(int i = 0 ; i < answer.length ; i++) {
+      for(int j = 0 ; j < answer[i].length ; j++) {
+        print("answer $i $j = ${answer[i][j]}");
+      }
+    }
+     */
 
     //resize puzzle
     if(answer.isEmpty) {
       print("answer is empty");
     }
-    List<List<SquareBox>> puzzle = initSquarePuzzle(answer.length - 1, answer[0].length / 2);
+    List<List<SquareBox>> puzzle = initSquarePuzzle(answer[0].length, answer.length ~/ 2);
+    print("puzzle SquareBox => row ${puzzle.length}, col ${puzzle[0].length}");
     List<Widget> columnChildren = [];
 
     //marking answer line
@@ -182,30 +204,364 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
     }
     //marking number with answer
     setNumWithAnswer(puzzle);
-    clearLine(puzzle);
+    //setDefaultLineStep1(puzzle);
+    clearLineForStart(puzzle);
 
     return columnChildren;
   }
 
-  static void clearLine(List<List<SquareBox>> puzzle) {
+  //set(0,0) & find num 0 in all field
+  static void setDefaultLineStep1(List<List<SquareBox>> puzzle) {
+    int inValidColor = -2;
+    List<List<int>> points = [];
+
+    //set (0,0)
+    if(puzzle[0][0].num == 0) {
+      puzzle[0][0].up = inValidColor;
+      puzzle[0][0].down = inValidColor;
+      puzzle[0][0].left = inValidColor;
+      puzzle[0][0].right = inValidColor;
+
+      points.add([0,0]);
+    }
+    else if(puzzle[0][0].num == 1) {
+      puzzle[0][0].up = inValidColor;
+      puzzle[0][0].down = 0;
+      puzzle[0][0].left = inValidColor;
+      puzzle[0][0].right = 0;
+
+      points.add([0,0]);
+    }
+    else {
+      puzzle[0][0].up = 0;
+      puzzle[0][0].down = 0;
+      puzzle[0][0].left = 0;
+      puzzle[0][0].right = 0;
+    }
+
     for(int i = 0 ; i < puzzle.length ; i++) {
       for(int j = 0 ; j < puzzle[i].length ; j++) {
+        if(puzzle[i][j].num == 0) {
+          if(i != 0 && j != 0) {
+            puzzle[i - 1][j].down = inValidColor;
+            puzzle[i][j].down = inValidColor;
+            puzzle[i][j - 1].right = inValidColor;
+            puzzle[i][j].right = inValidColor;
+
+            points.add([i,j]);
+          }
+          //first row
+          else if(i == 0 && j != 0) {
+            puzzle[i][j].up = inValidColor;
+            puzzle[i][j].down = inValidColor;
+            puzzle[i][j - 1].right = inValidColor;
+            puzzle[i][j].right = inValidColor;
+
+            points.add([i,j]);
+          }
+          //first col
+          else if(i != 0 && j == 0) {
+            puzzle[i - 1][j].down = inValidColor;
+            puzzle[i][j].down = inValidColor;
+            puzzle[i][j].left = inValidColor;
+            puzzle[i][j].right = inValidColor;
+
+            points.add([i,j]);
+          }
+          else {
+            //(0,0) is already set
+          }
+        }
+      }
+    }
+
+    setDefaultLineStep2(puzzle, points);
+  }
+
+  static void setDefaultLineStep2(List<List<SquareBox>> puzzle, List<List<int>> points) {
+    int inValidColor = -2;
+    int startRow = 0, startCol = 1;
+    int endRow = 1, endCol = 0;
+    List<List<int>> newPoints = [];
+
+    for(int i = 0 ; i < points.length ; i++) {
+      //check 2 dir
+      if(points[i] == [0,0] || points[i] == [puzzle.length - 1,0]
+          || points[i] == [0, puzzle[puzzle.length - 1].length - 1]
+          || points[i] == [puzzle.length - 1, puzzle[puzzle.length - 1].length - 1]) {
+        //down, right
+        if(points[i] == [0,0]) {
+          if(puzzle[0][0].right == inValidColor) {
+            puzzle[0][1].up == inValidColor;
+          }
+          if(puzzle[0][0].left == inValidColor && puzzle[0][0].down == inValidColor) {
+            puzzle[0][1].left == inValidColor;
+          }
+        }
+        //up, right
+        else if(points[i] == [puzzle.length - 1,0]) {
+
+        }
+        //down, left
+        else if(points[i] == [0, puzzle[puzzle.length - 1].length - 1]) {
+
+        }
+        //up, left
+        else {
+
+        }
+      }
+      //check 3 dir
+      else if(false) {
+
+      }
+    }
+  }
+
+  //set (0,0) & row 0's up & col 0's left
+  static void setDefaultLineStep123(List<List<SquareBox>> puzzle) {
+    int inValidColor = -2;
+    int cntEdge = 0;
+
+    //set (0,0)
+    if(puzzle[0][0].num == 0) {
+      puzzle[0][0].up = inValidColor;
+      puzzle[0][0].down = inValidColor;
+      puzzle[0][0].left = inValidColor;
+      puzzle[0][0].right = inValidColor;
+    }
+    else if(puzzle[0][0].num == 1) {
+      puzzle[0][0].up = inValidColor;
+      puzzle[0][0].down = 0;
+      puzzle[0][0].left = inValidColor;
+      puzzle[0][0].right = 0;
+    }
+    else {
+      puzzle[0][0].up = 0;
+      puzzle[0][0].down = 0;
+      puzzle[0][0].left = 0;
+      puzzle[0][0].right = 0;
+    }
+
+    int a = 0, b = 1;
+    int remainEdge = numOfEdge;
+    while(true) {
+      for(int i = a ; i < puzzle.length ; i++) {
+        //row axis's up (left to right)
+        for(int j = b ; j < puzzle[i].length ; j++) {
+          if(puzzle[i][j].num == 0) {
+            puzzle[i][j].up = inValidColor;
+            puzzle[i][j].down = inValidColor;
+            puzzle[i][j - 1].right = inValidColor;
+            puzzle[i][j].right = inValidColor;
+          }
+          else {
+            if(i >= 1) {
+              if(puzzle[i][j - 1].right == inValidColor &&
+                  puzzle[i][j - 1].up == inValidColor &&
+                  puzzle[i - 1][j].right == inValidColor) {
+                puzzle[i][j].up = inValidColor;
+                remainEdge--;
+                print("set [$i][$j].up with left to right -1");
+              }
+            }
+            else {  // i < 1
+              if(puzzle[i][j - 1].right == inValidColor &&
+                  puzzle[i][j - 1].up == inValidColor) {
+                puzzle[i][j].up = inValidColor;
+                remainEdge--;
+                print("set [$i][$j].up with left to right -1");
+              }
+            }
+          }
+        }
+        //row axis's up (right to left)
+        for(int j = puzzle[i].length - 1 ; j >= b ; j--) {
+          if(puzzle[i][j].num <= 1) {
+            //check j+1's left
+            if(j < puzzle[i].length - 1) {
+              if(puzzle[i][j + 1].left == inValidColor) {
+                puzzle[i][j].up = inValidColor;
+              }
+            }
+            //j+1's left is not exist
+            else {
+              puzzle[i][j].up = inValidColor;
+            }
+          }
+          else {
+            puzzle[i][j].up = 0;
+          }
+
+
+          if(j < puzzle[i].length - 1) {
+            if(puzzle[i][j].num <= 1) {
+              puzzle[i][j].up = inValidColor;
+            }
+            else {
+
+            }
+          }
+          else {
+
+          }
+
+          if(puzzle[i][j - 1].right == inValidColor) {
+            puzzle[i][j].up = inValidColor;
+            remainEdge--;
+            print("set [$i][$j].up with right to left -1");
+          }
+        }
+        /*
+        //row axis's left and right
+        for(int j = b ; j < puzzle[i].length ; j++) {
+          //judge left
+          if(puzzle[i][j - 1].up == inValidColor && puzzle[i][j].up == inValidColor) {
+            puzzle[i][j].left = inValidColor;
+            print("set [$i][$j].left -1");
+          }
+          //judge right
+          if(j < puzzle[i].length) {
+            if(puzzle[i][j].up == inValidColor && puzzle[i][j + 1].up == inValidColor) {
+              puzzle[i][j].right = inValidColor;
+              print("set [$i][$j].right -1");
+            }
+          }
+          else {
+            if(puzzle[i][j].up == inValidColor) {
+              puzzle[i][j].right = inValidColor;
+              print("set [$i][$j].right -1");
+            }
+          }
+
+        }
+         */
+
+        break;
+      }
+
+      break;
+      //const col axis
+    }
+
+    /*
+    for(int i = 0 ; i < puzzle.length ; i++) {
+      for (int j = 0; j < puzzle[i].length; j++) {
+        if(i != 0 && j != 0) {
+
+        }
+        //set col 0's left
+        else if(i != 0 && j == 0) {
+
+        }
+        //set row 0's up
+        else if(i == 0 && j != 0) {
+          if(puzzle[i][j].num == 0) {
+            puzzle[i][j].up = inValidColor;
+            puzzle[i][j].down = inValidColor;
+            puzzle[i][j].right = inValidColor;
+          }
+          else {
+            if(puzzle[i][j - 1].right == -1) {
+              cntEdge++;
+            }
+            if()
+          }
+        }
+        else {
+          if(puzzle[i][j].num == 0) {
+            puzzle[i][j].up = inValidColor;
+            puzzle[i][j].down = inValidColor;
+            puzzle[i][j].left = inValidColor;
+            puzzle[i][j].right = inValidColor;
+          }
+          else {
+            if(puzzle[i][j].num == 1) {
+              puzzle[i][j].up = inValidColor;
+              puzzle[i][j].left = inValidColor;
+              continue;
+            }
+
+            puzzle[i][j].up = 0;
+            puzzle[i][j].down = 0;
+            puzzle[i][j].left = 0;
+            puzzle[i][j].right = 0;
+          }
+        }
+      }
+    }
+     */
+  }
+
+  //set line with condition
+  static void setDefaultLine(List<List<SquareBox>> puzzle) {
+    int count = 0;
+
+    for(int i = 0 ; i < puzzle.length ; i++) {
+      for(int j = 0 ; j < puzzle[i].length ; j++) {
+        count = 0;
+
         if(i != 0 && j != 0) {
           puzzle[i][j].down = 0;
           puzzle[i][j].right = 0;
-        } else if(i != 0 && j == 0) {
-          puzzle[i][j].down = 0;
-          puzzle[i][j].left = 0;
-          puzzle[i][j].right = 0;
-        } else if(i == 0 && j != 0) {
-          puzzle[i][j].up = 0;
-          puzzle[i][j].down = 0;
-          puzzle[i][j].right = 0;
-        } else {
-          puzzle[i][j].up = 0;
-          puzzle[i][j].down = 0;
-          puzzle[i][j].left = 0;
-          puzzle[i][j].right = 0;
+        }
+        //set col 0's left
+        else if(i != 0 && j == 0) {
+          if(puzzle[i][j].num == 0) {
+            puzzle[i][j].down = 0;
+            puzzle[i][j].left = 0;
+            puzzle[i][j].right = 0;
+            continue;
+          }
+          else {
+            if(puzzle[i - 1][j].down == -1) {
+              puzzle[i][j].left = -1;
+            }
+          }
+        }
+        //set row 0's up
+        else if(i == 0 && j != 0) {
+          if(puzzle[i][j].num == 0) {
+            puzzle[i][j].up = -1;
+            puzzle[i][j].down = -1;
+            puzzle[i][j].right = -1;
+            continue;
+          }
+          else {
+            if(puzzle[i][j - 1].right == -1) {
+              count++;
+            }
+            //already set
+            if(numOfEdge == puzzle[i][j].num + count) {
+              puzzle[i][j].up = 0;
+              puzzle[i][j].down = 0;
+              puzzle[i][j].right = 0;
+            }
+            //need setting -1
+            else {
+              //set `up`
+              if(puzzle[i][j - 1].right == -1) {
+                puzzle[i][j].up = -1;
+              }
+              else {
+                puzzle[i][j].up = 0;
+              }
+            }
+          }
+        }
+        else {
+          if(puzzle[i][j].num == 0) {
+            puzzle[i][j].up = -1;
+            puzzle[i][j].down = -1;
+            puzzle[i][j].left = -1;
+            puzzle[i][j].right = -1;
+          }
+          else {
+            puzzle[i][j].up = 0;
+            puzzle[i][j].down = 0;
+            puzzle[i][j].left = 0;
+            puzzle[i][j].right = 0;
+          }
         }
       }
     }
@@ -252,6 +608,7 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
     for(int i = 0 ; i < answer.length ; i++) {      //10 ,11, 10, 11...
       for (int j = 0; j < answer[i].length; j++) {  //3, 5, 7, 9...
         lineType = answer[i][j];
+        //print("list $i $j / $lineType");
 
         if(i <= 2 && j <= 1) {  //up, down, left, right 모두 존재
           if(i == 0) {
@@ -295,6 +652,33 @@ class GameSceneStateSquare extends State<GameSceneSquare> {
           } else {
             puzzle[(i - 1) ~/ 2][j - 1].right = lineType;
           }
+        }
+      }
+    }
+  }
+
+  static void clearLineForStart(List<List<SquareBox>> puzzle) {
+    for(int i = 0 ; i < puzzle.length ; i++) {
+      for(int j = 0 ; j < puzzle[i].length ; j++) {
+        if(i != 0 && j != 0) {
+          puzzle[i][j].down = 0;
+          puzzle[i][j].right = 0;
+        }
+        else if(i == 0 && j != 0) {
+          puzzle[i][j].up = 0;
+          puzzle[i][j].down = 0;
+          puzzle[i][j].right = 0;
+        }
+        else if(i != 0 && j == 0) {
+          puzzle[i][j].down = 0;
+          puzzle[i][j].left = 0;
+          puzzle[i][j].right = 0;
+        }
+        else {
+          puzzle[i][j].up = 0;
+          puzzle[i][j].down = 0;
+          puzzle[i][j].left = 0;
+          puzzle[i][j].right = 0;
         }
       }
     }
